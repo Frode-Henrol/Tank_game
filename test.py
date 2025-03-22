36d973deb18d6512427eb96ff17343a6ba65da87
Frode-Henrol <glock.linstow@gmail.com>
2025-03-22 12:06:53 +0100

    new refactors

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-
+import pygame as pg
+import utils.helper_functions as hf
 import numpy as np
+import triangle as tr
+import heapq
+from collections import defaultdict
+import time 
+import math
+from map_grid import MapGrid
 
-map_grid = np.zeros([5,5])
 
-print(map_grid)
+polygons, units = hf.load_map_data(r"map_files\map_test1.txt")
 
+
+grid = MapGrid(polygons, 50)
+
+print(grid.get_mapgrid_dict())

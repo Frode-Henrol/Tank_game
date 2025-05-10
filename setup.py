edf4be17cc92e7a78cd8ac925fa5a564e7807249
Frode-Henrol <glock.linstow@gmail.com>
2025-05-10 11:20:43 +0200

    fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Tank game", 
-    version="1.0.0",
+    version="0.1.0",
     description="Tank game inspired by wii tanks",
     author="Frode Henrol",
     packages=find_packages(),
